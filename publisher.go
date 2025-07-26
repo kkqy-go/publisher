139e2f0b7120ed3d73c82d2cbeba963a7538053d
kkqy <kuokongqingyun@gmail.com>
2025-07-26 08:06:53 +0800

    update

--- publisher.go
+++ publisher.go
@@ -25,6 +25,8 @@ func (s *Subscriber[T]) Publish(ctx context.Context, data T) bool {
 type Publisher[T any] struct {
 	subscribers              generic.SyncMap[*Subscriber[T], struct{}]
 	newSubscriberSubscribers generic.SyncMap[*Subscriber[*Subscriber[T]], struct{}]
+
+	subscribeBufLen int
 }
 
 func (p *Publisher[T]) PublishChannel(ctx context.Context, pubChan chan T) {
@@ -40,7 +42,6 @@ func (p *Publisher[T]) PublishChannel(ctx context.Context, pubChan chan T) {
 		}
 	}
 }
-
 func (p *Publisher[T]) Publish(pubCtx context.Context, data T) {
 	p.subscribers.Range(func(subscriber *Subscriber[T], _ struct{}) bool {
 		ok := subscriber.Publish(pubCtx, data)
@@ -50,9 +51,8 @@ func (p *Publisher[T]) Publish(pubCtx context.Context, data T) {
 		return true
 	})
 }
-
 func (p *Publisher[T]) Subscribe(subCtx context.Context) (<-chan T, error) {
-	ch := make(chan T, 1)
+	ch := make(chan T, p.subscribeBufLen)
 	subscriber := &Subscriber[T]{
 		subCtx: subCtx,
 		ch:     ch,
@@ -79,7 +79,18 @@ func (p *Publisher[T]) NewSubscriberChan(ctx context.Context) (<-chan *Subscribe
 	return ch, nil
 }
 
-func NewPublisher[T any]() *Publisher[T] {
+type PublisherOption[T any] func(p *Publisher[T])
+
+func WithSubscriberBufLen[T any](bufLen int) PublisherOption[T] {
+	return func(p *Publisher[T]) {
+		p.subscribeBufLen = bufLen
+	}
+}
+
+func NewPublisher[T any](opts ...PublisherOption[T]) *Publisher[T] {
 	p := &Publisher[T]{}
+	for _, opt := range opts {
+		opt(p)
+	}
 	return p
 }
